f76ff9fa334570c4fb62a12f23fe256c231ba10e
pedouard2 <pedouard2@gmail.com>
2023-02-18 11:34:54 -0500

    Added flask config

--- api/api.py
+++ api/api.py
@@ -1,8 +1,17 @@
 import pronouncing
 import time
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+# Load the default configuration
+app.config.from_object('config')
+
+# Load the configuration from the instance folder
+app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
 
 @app.route('/api/time')
 def get_current_time():
