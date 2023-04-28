2bfce0a97d644f11ec089fc5cbbf13f6ff7fcfea
pedouard2 <pedouard2@gmail.com>
2023-04-27 23:39:20 -0400

    Fixed promises

--- src/rhyme/index.js
+++ src/rhyme/index.js
@@ -2,53 +2,37 @@ import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 
 const getRhymingParts = async (wordList) => {
-  let res = [];
+  let promises = [];
   for (let word of wordList) {
-    try {
-      const response = await axios.get(
-        `http://localhost:5000/v1/words/${word}/rhyming-parts`
-      );
-      res.push({ word: response });
-    } catch (error) {
-    //   console.log(error);
-    }
+    const response = await axios.get(
+      `http://localhost:5000/v1/words/${word}/rhyming-parts`
+    );
+    promises.push(response);
   }
-  return res;
+  let resp = await Promise.all(promises);
+
+  resp = resp.map((entry) => entry.data);
+
+  return resp;
 };
 const seenWords = new Set();
 
 const Rhymes = ({ rhymes }) => {
-
   const [apiResponse, setApiResponse] = useState([]);
 
-  let words = rhymes.split(" ") 
-  // words that are new that havent been seen
-  let wordsDiff = new Set([...words].filter(x => !seenWords.has(x)));
-  // api call to new words
-  let temp = getRhymingParts(wordsDiff)
-  // add recently seen words to seen
-  wordsDiff.forEach(seenWords.add, seenWords)
+  const wl = rhymes.split(" ");
+  const res = getRhymingParts(wl);
 
-
+  res.then((values) => {
+    // setApiResponse(values)
+    console.log(values)
+  });
 
   return (
     <div>
-      <p>
-        {/* {rhymes} */}
-        {wordsDiff}
-        {/* {rhymes.map((item, index) => (
-          <span key={index}> {item}</span>
-        ))} */}
-      </p>
+      <p>{rhymes}</p>
     </div>
   );
 };
 
 export default Rhymes;
-
-
-  //     for(let i in rhymes.split(" ")) {
-  // // use state to populate itemlsit for each axios call then item list to map and return
-  // // empty list of rhyming part objects
-  // // map paragraph to the map
-  //         }
