cd12896a25bf9e7696f4b1685b30d4ac3dff718b
pedouard2 <pedouard2@gmail.com>
2023-04-27 23:39:56 -0400

    removed test list reverted mirror text

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -3,52 +3,42 @@ import "./main-page.css";
 import Header from "./header";
 import Rhymes from "../rhyme";
 
-let rhymeList = [
-  {
-    rhyming_parts: ["AA1 ", "AO1 "],
-    word: "top",
-  },
-  {
-    rhyming_parts: ["AH1 "],
-    word: "run",
-  },
-  {
-    rhyming_parts: ["AH1 ER0"],
-    word: "hunter",
-  },
-  {
-    rhyming_parts: [],
-    word: " ",
-  },
-  {
-    rhyming_parts: ["IH1 ", "IH2 "],
-    word: "permit",
-  },
-  {
-    rhyming_parts: [],
-    word: "dfnjsdkl;fna",
-  },
-];
-
 function App() {
-
-  const [bodyText, setBodyText] = useState("")
+  const [bodyText, setBodyText] = useState("");
 
   return (
     <div className="container">
       <Header />
-      <div className="preview">
-        <h2>Preview: {bodyText}</h2>
-      </div>
-      <div className="container">
-        <textarea
-        name="bodyText" 
-        onChange={event => setBodyText(event.target.value)}>
-        </textarea> 
+
+      <div>
+        <div>
+          <textarea
+          name="bodyText" 
+          onChange={event => setBodyText(event.target.value)}>
+          </textarea>
+        </div>
+
+        <div>
         <Rhymes rhymes={bodyText} />
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <div className="container">
+<textarea
+  name="bodyText"
+  onSubmit={(event) => setBodyText(event.target.value)}
+></textarea>
+<button type="submit">submit</button>
+
+<div className="preview">
+  <h2>Preview: {bodyText}</h2>
+</div>
+{/* <Rhymes rhymes={bodyText} /> */
+}
+// </div> */}
