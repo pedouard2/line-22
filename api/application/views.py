4ad7ec2a64dc68e6b5471f253d083ecb7a6ab072
pedouard2 <pedouard2@gmail.com>
2023-05-14 21:46:23 -0400

    Added * filter

--- api/application/views.py
+++ api/application/views.py
@@ -19,15 +19,26 @@ def get_current_time():
     return {'time': time.time()}
 
 def get_word_entry(wrd):
-    res = Word.query.filter_by(word=wrd).all()
-    if not res:
-        return {}
-    return {
-        "word": wrd,
-        "rhyming_parts": res[0].rhyming_part,
-        "syllables": res[0].syllable,
-        "children" : [add_child(wrd,w) for w in res[1:] if len(res) > 1]
-        }
+    if wrd == "*":
+        resp = Word.query.filter(True).all()
+        seen = set()
+        res = []
+        for word in resp:
+            if word.word not in seen:
+                seen.add(word.word)
+                word_entry = get_word_entry(word.word)
+                res.append(word_entry)
+        return res
+    else:    
+        res = Word.query.filter_by(word=wrd).all()
+        if not res:
+            return {}
+        return {
+            "word": wrd,
+            "rhyming_parts": res[0].rhyming_part,
+            "syllables": res[0].syllable,
+            "children" : [add_child(wrd,w) for w in res[1:] if len(res) > 1]
+            }
 
 def add_child(word, word_entry):
     return {
@@ -37,11 +48,15 @@ def add_child(word, word_entry):
     }
 @v1.route('/words/<word>/rhyming-parts', methods=["GET"])
 def rhyming_part(word):
+    # args = request.args
+    # query = args.get('query')
     #TODO escape chars for sql injection
     if request.method == "GET":
         #TODO pattern match for space -> return space
         if Word.query.filter_by(word=word).first():
             return get_word_entry(word)
+        # elif query and query == "all":
+        #     return get_word_entry("*")
         else:
             try:
                 phones = pronouncing.phones_for_word(word)
